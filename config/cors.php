200dcb5da01e29df00e56d9a5fa1dbbd4f665b6e
karamAnstas123 <karam.anstas.2003@gmail.com>
2025-05-21 20:40:49 +0300

    made changes in the config file cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'email/verify/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api/verify-email/*'],
 
     'allowed_methods' => ['*'],
 
