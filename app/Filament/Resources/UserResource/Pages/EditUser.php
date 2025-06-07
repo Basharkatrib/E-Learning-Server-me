70638cdefda0d3cfa21388425b143dd2fd4dd577
Your Name <you@example.com>
2025-06-08 01:51:28 +0300

    role notifications

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,10 +5,31 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use App\Events\RoleUpdated;
+use Illuminate\Database\Eloquent\Model;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $oldRole = $record->getOriginal('role');
+
+        $record->update($data);
+
+        if ($oldRole !== $data['role']) {
+            $eventData = [
+                'message' => "User {$record->name}'s role has been changed from {$oldRole} to {$data['role']}",
+                'user_name' => $record->name,
+                'old_role' => $oldRole,
+                'new_role' => $data['role']
+            ];
+            event(new RoleUpdated($eventData));
+        }
+
+        return $record;
+    }
 
     protected function getHeaderActions(): array
     {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Events\MyEvent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/trigger', function () {
+    $data = [
+        'message' => 'Hii'
+    ];
+    broadcast(new MyEvent($data));
+    return 'Event Triggered';
 });
