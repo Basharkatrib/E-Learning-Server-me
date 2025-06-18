81d8a9432fd94dc4dc3a3f0c6edfd811c251df10
Your Name <you@example.com>
2025-06-18 18:35:00 +0300

    VideoSeeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
             RatingSeeder::class,
             SectionSeeder::class,
             BenefitsCourseSeeder::class,
+            VideosSeeder::class,
         ]);
     }
 }
