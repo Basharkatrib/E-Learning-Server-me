af1730187a9de63229e3709d1b0326fd5e5c8d8b
Your Name <you@example.com>
2025-06-19 02:03:54 +0300

    Provider edit3

--- app/Filament/Resources/CourseFaqResource.php
+++ app/Filament/Resources/CourseFaqResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CourseFaqResource\Pages;
 use App\Filament\Resources\CourseFaqResource\RelationManagers;
-use App\Models\CourseFaq;
+use App\Models\CourseFAQ;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -18,7 +18,7 @@ use App\Filament\Traits\HasRoleBasedAccess;
 class CourseFaqResource extends Resource
 {
     use HasRoleBasedAccess;
-    protected static ?string $model = CourseFaq::class;
+    protected static ?string $model = CourseFAQ::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-question-mark-circle';
 
