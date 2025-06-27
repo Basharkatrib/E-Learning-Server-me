11cc8d3fd8388e0bcf1611d6bcd787bf24db65b2
Your Name <you@example.com>
2025-06-27 13:53:59 +0300

    Fix dashboard images2

--- app/Filament/Resources/CourseResource.php
+++ app/Filament/Resources/CourseResource.php
@@ -79,12 +79,15 @@ class CourseResource extends Resource
                         if (is_array($state)) {
                             $state = reset($state);
                         }
+                
                         if (is_string($state) && str_starts_with($state, 'http')) {
                             return $state;
                         }
+                
                         if (is_string($state)) {
-                            return \Storage::disk('cloudinary')->url($state);
+                            return str_replace('http://', 'https://', \Storage::disk('cloudinary')->url($state));
                         }
+                
                         return $state;
                     }),
                 Forms\Components\Select::make('default_language')
